f6f2f223d58f4da12fa8e17dfe02b4b8ea4b7f5f
Sally XU <sallyspinnet@gmail.com>
2023-10-28 00:49:10 +0200

    added scripts for creating counterfactual corpus

---
+++ scripts/counterfactual.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+language="en"
+timestamp=$(date +%s)
+
+Help()
+{
+   # Display Help
+   echo "Script to run counterfactual corpus creation."
+   echo
+   echo "Syntax: data.sh [-l|p|f|o]"
+   echo "options:"
+   echo "l     Language code to create counterfactual corpus. Default: en"
+   echo "p     Pair <X,Y> to swap. e.g.: VO, ADP_NP. Default: VO"
+   echo "f     Filename of CONLLU data. Default: ./data/wiki40b-random/regular/en_sample.conllu"
+   echo "o     Output file of plain reordered text that has a specific <X,Y> pair reordered. Default: ./data/wiki40b-random/regular/en_ov_test.txt"
+
+   echo
+}
+
+while getopts "l:t:s:v:o:b:dh" option; do
+  case $option in
+    l)
+      language="$OPTARG"
+      ;;
+    p)
+      swap_pair="$OPTARG"
+      ;;
+    f)
+      conllu_file="$OPTARG"
+      ;;
+    o)
+      output_file="$OPTARG"
+      ;;
+    h)
+      Help
+      exit
+      ;;
+    *)
+      echo "Usage: $0 [-l language]"
+      exit 1
+      ;;
+  esac
+done
+
+echo
+echo "Language code: $language"
+echo "Pair to reverse: $swap_pair"
+echo "CoNLLU parse file: $conllu_file"
+echo "Counterfactual corpus stored at: $output_file"
+
+# Set default value if ${TRAIN_SAMPLE} is not set or empty
+# TRAIN_SAMPLE="${TRAIN_SAMPLE:-default_value}"
+
+LANGUAGE=${language} SWAP_PAIR="${swap_pair:-'VO'}" \
+CONLLU_FILE="${conllu_file:-'./data/wiki40b-random/regular/en_sample.conllu'}" \
+OUTPUT_FILE="${output_file:-'./data/wiki40b-random/regular/en_ov_test.txt'}" \
+sbatch --output="./logs/counterfactual/rev_${language}_${swap_pair}_${timestamp}.out" scripts/counterfactual.euler
---
+++ src/counterfactual/check_diff.py
@@ -0,0 +1,18 @@
+def compare_text_files(file1, file2, output_file):
+    with open(file1, 'r') as f1, open(file2, 'r') as f2:
+        lines1 = f1.readlines()
+        lines2 = f2.readlines()
+
+    # Compare the lines and find the differences
+    differences = [f"{line1.strip()} | {line2.strip()}" for line1, line2 in zip(lines1, lines2) if line1 != line2]
+
+    # Write the differences to the output file
+    with open(output_file, 'w') as output:
+        output.write('\n'.join(differences))
+
+# Usage:
+if __name__ == "__main__":
+    file1 = './data/wiki40b-random/regular/en_ov.txt'
+    file2 = './data/wiki40b-random/regular/en_ov_test.txt'
+    output_file = './diff.txt'
+    compare_text_files(file1, file2, output_file)
