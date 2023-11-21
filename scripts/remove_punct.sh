fcae0e500742f4023c4b80186ce67217ffba2f14
Sally XU <sallyspinnet@gmail.com>
2023-11-21 16:37:41 +0100

    minor fix

--- scripts/remove_punct.sh
+++ scripts/remove_punct.sh
@@ -48,6 +48,6 @@ echo "Output directory: $output_dir"
 # Set default value if ${TRAIN_SAMPLE} is not set or empty
 # TRAIN_SAMPLE="${TRAIN_SAMPLE:-default_value}"
 
-LANGUAGE=${language} DATA_DIR="${data_dir:-'./data/wiki40b-txt/'}" \
-PARSE_DIR="${parse_dir:-'./data/wiki40b-baseline/'}" \
-sbatch --output="./logs/parsing/dep_${language}_${timestamp}.out" scripts/remove_punct.euler
+LANGUAGE=${language} DATA_DIR="${data_dir:-'./data/wiki40b-txt'}" \
+PARSE_DIR="${parse_dir:-'./data/wiki40b-baseline'}" \
+sbatch --output="./logs/baseline/remove_${language}_${timestamp}.out" scripts/remove_punct.euler
